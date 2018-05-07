b260fef1dfd681134242100d33d80db394205512
Shiv.Singh <Shivfgs@gmail.com>
2018-05-08 02:27:57 +0530

    tt

--- TestAutoDemo/src/com/tad/runner/TestRunner.java
+++ TestAutoDemo/src/com/tad/runner/TestRunner.java
@@ -5,7 +5,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions( features="com/tad/features",
 glue="com.tad.stepdefinition",
-plugin={"pretty","html:reports/test-report"})
+plugin={"pretty","html:reports/test-report"},
+tags= {"@SmokeTest,@RegressionTest"})
 
 public class TestRunner {
 	
